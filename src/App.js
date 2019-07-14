892d56b1f780e77d1d3c63137fdf5026c1f3abe2
Doina Ieseanu <doina@utexas.edu>
2019-07-14 09:20:10 +0100

    updating stripe integration, updating favicon and title, updating code styling in various components

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
-
 	unsubscribeFromAuth = null;
 
 	componentDidMount(){
@@ -31,9 +33,8 @@ class App extends React.Component {
 					});
 				});
 			}
-			else {
-				setCurrentUser(userAuth);
-			}
+			
+			setCurrentUser(userAuth);
 		});
 	}
 
@@ -46,8 +47,8 @@ class App extends React.Component {
 	    <div>
 	    	<Header />
 		    <Switch>
-		     	<Route exact path='/' component = {HomePage} />
-		     	<Route exact path='/shop' component = {ShopPage} />
+		     	<Route exact path='/' component={HomePage} />
+		     	<Route path='/shop' component = {ShopPage} />
 		     	<Route exact path='/checkout' component = {CheckoutPage} />
 		     	<Route 
 		     		exact
@@ -55,7 +56,7 @@ class App extends React.Component {
 		     		render={() => 
 		     			this.props.currentUser ? (
 		     				<Redirect to='/' />
-		     			): (
+		     			) : (
 		     				<SignInAndSignUpPage />
 		     			)
 		     		} 
@@ -63,18 +64,19 @@ class App extends React.Component {
 		     </Switch>
 	    </div>
   		);
-  	};
+  	}
 }
 
-const mapStateToProps = createStructuredSelector ({
-	currentUser: selectCurrentUser
-})
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
 
 const mapDispatchToProps = dispatch => ({
-	setCurrentUser: user => dispatch(setCurrentUser(user))
+  setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-	mapStateToProps, 
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(App);
+
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CheckoutItem from '../../components/checkout-item/checkout-item.components';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 
@@ -36,6 +37,12 @@ const CheckoutPage = ({cartItems, total }) => (
 		<div className='total'>
 			<span>Total: ${total}</span>
 		</div>
+		<div className='test-warning'>
+			*Please use the following test credit card for payment
+			<br />
+			4242 4242 4242 4242 - Exp: 01/20 - CCV: 123
+		</div>
+		<StripeCheckoutButton price={total} />
 	</div>
 )
 
